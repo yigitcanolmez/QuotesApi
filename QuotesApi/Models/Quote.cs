91973a41f4e4bb9eb16fd7a371262d2e923de060
yigitcanolmez <yigitcanolmez@outlook.com>
2023-08-27 13:18:16 +0300

    Quote model created

---
+++ QuotesApi/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace QuotesApi.Models
+{
+	public class Quote
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Description { get; set; }
+
+
+	}
+}
+
