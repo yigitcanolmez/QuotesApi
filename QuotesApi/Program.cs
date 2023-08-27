6fe977e7ddd2bcebb4e1ae664b677f7dd68c2e07
yigitcanolmez <yigitcanolmez@outlook.com>
2023-08-27 12:24:26 +0300

    .

--- QuotesApi/Program.cs
+++ QuotesApi/Program.cs
@@ -1,19 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
