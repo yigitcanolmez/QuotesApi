cbbe9ee01217f627d4129ad48f5e762c7425ca38
Yiğitcan Ölmez <90285509+yigitcanolmez@users.noreply.github.com>
2023-09-08 18:28:49 +0300

    Update Quote.cs

--- QuotesApi/Models/Quote.cs
+++ QuotesApi/Models/Quote.cs
@@ -10,8 +10,8 @@ namespace QuotesApi.Models
 		public string Title { get; set; }
 		public string Author { get; set; }
 		public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-
+                public DateTime CreatedAt { get; set; }
+ 
 
     }
 }
